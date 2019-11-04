00630ef0b2b79ad4a1503037c7215a61d7d20840
Tecnico em Informatica 2018.5 <16121742018.5@senacrjedu.local>
2019-11-04 09:12:46 -0300

    foi alterado o perfil

--- src/app/model/perfil.ts
+++ src/app/model/perfil.ts
@@ -1,15 +1,21 @@
 export class Perfil{
     
-    nome : string;
-    sobrenome : string;
-    email: string;
-    telefone :string
+    id : string;
+    nomeCompleto : string;
+    senha: string;
+    email : string;
+    dataNascimento : string;
+    telefone : string;
+    cpf : string;
 
-    setPerfil(obj : any){
-        this.nome = obj.nome;
-        this.sobrenome = obj.sobrenome;
+    setPerfil(obj : any, id : any){
+        this.id = id;
+        this.nomeCompleto = obj.nomeCompleto;
+        this.senha = obj.senha;
         this.email = obj.email;
+        this.dataNascimento = obj.dataNascimento;
         this.telefone = obj.telefone;
+        this.cpf = obj.cpf;
 
     }
 }
