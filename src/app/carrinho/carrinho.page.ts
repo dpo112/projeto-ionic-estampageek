3f919650b073cd4937fb65911b7d850ddc290d62
Gabrel <gabrielricardo300499@gmail.com>
2019-11-22 11:22:12 -0300

    carrinho

--- src/app/carrinho/carrinho.page.ts
+++ src/app/carrinho/carrinho.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Produto } from '../model/produto';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ToastController, AlertController } from '@ionic/angular';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-carrinho',
@@ -7,9 +13,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarrinhoPage implements OnInit {
 
-  constructor() { }
+  listaProduto :Produto[] = [];
 
-  ngOnInit() {
+  constructor(private db: AngularFirestore,
+     private router : Router,private fireStorage : AngularFireStorage) {
+
+}
+
+ngOnInit(){
+  this.db.collection('produto').snapshotChanges().subscribe(response=>{
+  this.listaProduto = [];
+    response.forEach(doc=>{
+    let p = new Produto();
+    p.setProduto(doc.payload.doc.data(),doc.payload.doc.id);
+        
+    let ref = this.fireStorage.storage.ref().child(`produto/${p.id}.jpg`);
+    ref.getDownloadURL().then(url => {
+    p.imagem = url;
+    this.listaProduto.push(p);
+  }).catch(()=>{
+    p.imagem = "../../assets/camisa-preta.png";
+    this.listaProduto.push(p);
+  })
+  
+  },err=>{
+    console.log(err);
+  })
+  console.log(this.listaProduto)
+  });
   }
 
+  goPage(idValue : string){
+    this.router.navigate(['produto-detalhes',{id : idValue}]);
+  }
 }
