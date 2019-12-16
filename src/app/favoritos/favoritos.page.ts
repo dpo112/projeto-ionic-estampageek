6588a288d72c796f19dcefb0479d675f8a994e71
Renanf97 <renanferreiradasilva06@gmail.com>
2019-12-16 10:51:19 -0300

    perfil

--- src/app/favoritos/favoritos.page.ts
+++ src/app/favoritos/favoritos.page.ts
@@ -1,80 +1,80 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { Produto } from '../model/produto';
-import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { ToastController, AlertController } from '@ionic/angular';
+import { ToastController, LoadingController } from '@ionic/angular';
 import { AngularFireStorage } from '@angular/fire/storage';
-import { Favoritos } from '../model/favoritos';
-import { FavoritosService } from '../services/favoritos.service';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+//import { Clientes } from '../model/clientes';
 
 @Component({
   selector: 'app-favoritos',
   templateUrl: './favoritos.page.html',
   styleUrls: ['./favoritos.page.scss'],
 })
-export class FavoritosPage implements OnInit {
+export class FavoritosPage implements OnInit{
+  
+  formGroup :FormGroup;  
+  //cliente : Clientes = new Clientes();
+  idUser : string;
+  
 
-  listaProduto :Produto[] = [];
-  favoritos : Favoritos = new Favoritos();
-  total : number;
+  constructor(private formB : FormBuilder,
+    private db: AngularFirestore,
+    private toastCtrl : ToastController,
+    private fireStorage : AngularFireStorage,
+    private loadingController : LoadingController,
+    private router : Router,
+    public afAuth: AngularFireAuth){
 
-  constructor(private db: AngularFirestore,
-     private router : Router,private fireStorage : AngularFireStorage,
-     private fav : FavoritosService) { // Injeta o Carrinho Service
-      
-      // Evitar erro de inicializar o carrinho null
-      this.favoritos.items = [];
-      
-      // Se o favoritos for nulo
-      if(this.fav.getFav()==null){
-        this.favoritos.items = []; // Cria um favoritos
-      }else{
-        this.favoritos = this.fav.getFav(); // pega o favoritos criado
-      }
-      
-      // Calcula o total
-      //this.total = this.fav.total();
+
+    this.formGroup = this.formB.group({
+      nome : ['',Validators.required],
+
+
+    });
+
+    this.afAuth.user.subscribe(resp =>{
+      this.idUser = resp.uid;
+      this.db.collection('favoritos').doc(this.idUser).get().subscribe(response=>{
+        console.log(response.data());
+
+        this.formGroup.controls['nome,0'].setValue(response.data().nomeCompleto);
+        
+        
+        // cliente.nomeComplento = response.data().nomeCompleto
+        // cliente.dataNascimento = response.data().dataNascimento
+        },err=>{
+          console.log(err);
+        })
+    });
+   }
+  
+  ngOnInit() {
+
 
       
-}
+    
 
-ngOnInit(){
-  if(this.fav.getFav()==null){
-    this.favoritos.items = [];
-  }else{
-    this.favoritos = this.fav.getFav();
+
   }
+
+  cadastrar(){
+    this.db.collection('favoritos').doc(this.idUser).set(this.formGroup.value).then(response =>{
+    this.presentToast();
+    }).catch(()=>{
+      console.log("Erro ao Cadastrar")
+    });
   
   }
-
-  goPage(idValue : string){
-    this.router.navigate(['produto-detalhes',{id : idValue}]);
+  
+  async presentToast(){
+    const toast = await this.toastCtrl.create({
+    message: 'Cadastro com sucesso',
+    duration: 2000  
+    });
+    toast.present();
+    this.router.navigate(['/home']) 
   }
 
-  goInicio(){
-    this.router.navigate(['home']);
-  }
-  goListar(){
-    this.router.navigate(['produto']);
-  }
-  goCart(){
-    this.router.navigate(['carrinho']);
-  }
-  goPerf(){
-    this.router.navigate(['perfil-lista']);
-  }
-
-  removeProduto(produto : Produto) : Favoritos{
-    let fav = this.fav.getFav();
-    // Verifica se existe o produto no carrinho
-
-    let position = fav.items.findIndex(x => x.produto.id==produto.id);
-    if(position!= -1){ // -1 -> Não existe
-        fav.items.splice(position,1);
-        this.favoritos.items.splice(position,1); // exclui o item carrinho
-    }
-    this.fav.setFav(fav); // atualiza o carrinho
-    return fav;
-}
 }
--- src/app/prod-feminino/prod-feminino.page.ts
+++ src/app/prod-feminino/prod-feminino.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Carrinho } from '../model/carrinho';
+import { Produto } from '../model/produto';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { CarrinhoService } from '../services/carrinho.service';
+import { Item } from '../model/item';
 
 @Component({
   selector: 'app-prod-feminino',
@@ -7,9 +14,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdFemininoPage implements OnInit {
 
-  constructor() { }
+  
+  listaProduto :Produto[] = [];
+  carrinho : Carrinho = new Carrinho();
+  imagem: any;
+  idImage: string;
+ 
 
-  ngOnInit() {
+
+
+  constructor(private db: AngularFirestore,
+     private router : Router,
+     private fireStorage : AngularFireStorage,
+     private car : CarrinhoService) {
+
+      this.carrinho.items = [];
+
+      if(this.car.getCart()==null){
+        this.carrinho.items = [];
+      }else{
+        this.carrinho = this.car.getCart();
+      }
+
+      
+
+}
+
+ngOnInit(){
+  this.db.collection('produto' , ref => ref.where('genero', '==', 'feminino')).snapshotChanges().subscribe(response=>{ 
+    this.listaProduto = []; // limpando a lista
+    // forEach equivalente ao for, percorre todos os elementos do firebase
+    // cada um se chama doc, ou seja, converter um doc em Produtos.
+    response.forEach(doc=>{ 
+      let p = new Produto(); // Cria um novo objeto Produtos
+      p.setProduto(doc.payload.doc.data(),doc.payload.doc.id); // coloca os dados do doc em Produtos
+      let ref = this.fireStorage.storage.ref().child(`produto/${p.id}.jpg`);
+      ref.getDownloadURL().then(url => {
+      p.imagem = url;
+      })
+      this.listaProduto.push(p); // adiciona este servicos a lista
+      console.log(this.listaProduto)
+    },err=>{ // Em caso de erro, executa esssa linha
+      console.log(err);
+    })
+  });
+
+  console.log(this.listaProduto)
   }
+
+  goDetalhePage(idValue : string){
+  this.router.navigate(['produto-detalhe',{id : idValue}]);
+}
+goInicio(){
+  this.router.navigate(['home']);
+}
+goListar(){
+  this.router.navigate(['produto']);
+}
+goCart(){
+  this.router.navigate(['carrinho']);
+}
+goPerf(){
+  this.router.navigate(['perfil-lista']);
+}
+
+addItem(p : Produto){
+
+
+  if(this.car.getCart()==null){
+    this.carrinho.items = [];
+  }else{
+    this.carrinho = this.car.getCart();
+  }
+
+  let item = new Item();
+  item.produto = p;
+  item.quantidade = 1;
+  
+  this.carrinho.items.push(item);
+
+  this.car.setCart(this.carrinho);
+
+  
+}
+
+
 
 }
